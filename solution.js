3b01cc01b78e44816fd8acba92e0d2a342d3a6ce
Irfan Hanjra <ihanjra@gmail.com>
2023-09-25 13:03:33 -0400

    added push and isEmpty methods to stack class

--- solution.js
+++ solution.js
@@ -1,6 +1,38 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(top = null) {
+    this.top = top
+  }
+
+  push(data) {
+    const newItem = new Node(data);
+    newItem.next = this.top;
+    this.top = newItem
+  }
+
+  isEmpty() {
+   return !this.top ? true : false;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+    this.max = 0;
+  }
+}
+
 module.exports = {
   Node,
   Queue,
