a9a93c1a4327a36ed6c02fc1342769386e16120b
jeremytjimenez <jeremytjimenez@gmail.com>
2023-10-02 01:15:08 -0400

    stacks class and methods completed

--- solution.js
+++ solution.js
@@ -1,6 +1,110 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(top = null) {
+    this.top = top;
+  }
+
+  push(data) {
+    const newItem = new Node(data);
+    newItem.next = this.top;
+    this.top = newItem;
+  }
+
+  size() {
+    let count = 0;
+    let currentNode = this.top;
+
+    while(currentNode) {
+      count++;
+      currentNode = currentNode.next;
+    }
+
+    return count;
+  }
+
+  pop() {
+    if (this.top === null) {
+      throw new Error("the stack is empty")
+    }
+
+    let item = this.top;
+
+    if (item) {
+      let newItem = item.next;
+      this.top = newItem;
+      return item;
+    }
+  }
+
+  isEmpty() {
+    return this.top === null;
+  }
+
+  findMin() {
+    let minValue = this.top.data;
+    let currentNode = this.top;
+
+    while (currentNode) {
+      if (currentNode.data < minValue) {
+        minValue = currentNode.data;
+        currentNode = currentNode.next;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    return minValue;
+  }
+
+  peek() {
+    if (this.top === null) {
+      throw new Error("The stack is empty");
+    }
+
+    return this.top;
+  }
+
+  sort() {
+    let stackArray = []
+
+    let currentNode = this.top
+
+    while (currentNode) {
+      stackArray.push(currentNode.data)
+      currentNode = this.top.next
+    }
+
+    stackArray.sort((a, b) => {
+      if (a > b) {
+        return 1
+      } else if (a < b) {
+        return -1
+      } else {
+        return 0
+      }
+    })
+
+    this.top = null
+
+    for (const value of stackArray) {
+      this.push(value)
+    }
+  }
+}
+
+class Queue {
+  
+}
+
 module.exports = {
   Node,
   Queue,
