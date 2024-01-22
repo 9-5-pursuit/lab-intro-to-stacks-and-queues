4eaac58652ca6b73216fc473364e7f313604f78c
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2024-01-22 10:03:57 -0500

    builds out skeleton of node, stack, and queue

--- solution.js
+++ solution.js
@@ -1,6 +1,38 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+  class Stack {
+    constructor(top = null) {
+      this.top = top;
+    }
+
+  }
+
+
+  class Queue {
+    consturctor() {
+      this.first = null;
+      this.last = null;
+      this.size = 0
+    }
+    
+
+  }
+
+
+
+
+
+
+
+
+
 module.exports = {
   Node,
   Queue,
